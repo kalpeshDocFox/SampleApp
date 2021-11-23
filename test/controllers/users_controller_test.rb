85f6bab5fb999e2a63734d91f6ef8bed8f296912
kalpeshmithal <kalpesh@docfox.io>
2021-11-23 15:00:03 +0200

    finalise chapter 6

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test "GET #new should render the new user page " do
     get users_new_url
     assert_response :success
   end
