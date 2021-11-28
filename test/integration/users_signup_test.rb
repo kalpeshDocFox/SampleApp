474827478edc65a5f4f0c4f9fd3691e22ca12820
kalpeshmithal <kalpesh@docfox.io>
2021-11-28 14:24:34 +0200

    fix pr comments

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
-
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "can successfully sign up a user with valid params" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: {
-                                  name: "",
-                                  email: "user@invalid",
-                                  password: "foo",
-                                  password_confirmation: "bar" } }
+        name: "",
+        email: "user@invalid",
+        password: "foo",
+        password_confirmation: "bar" } }
     end
     assert_template 'users/new'
   end
@@ -16,15 +15,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "test to confirm with valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-<<<<<<< HEAD
       post users_path, params: { user: {
-                                name: "Example User",
-                                email: "user@example.com",
-                                password: "password",
-                                confirm_password: "password" } }
-=======
-      post users_path, params: params
->>>>>>> 9794bf7 (Update test/integration/users_signup_test.rb)
+        name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        confirm_password: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
