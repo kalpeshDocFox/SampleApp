355cdc1ed8796c5e76fcef33378de30b6fb58815
kalpeshmithal <kalpesh@docfox.io>
2021-11-30 09:59:36 +0200

    pr comment changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
