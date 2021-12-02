9a15ba19febdf4a40c6569c99a2880d30f1017dd
kalpeshmithal <kalpesh@docfox.io>
2021-12-02 10:48:09 +0200

    rebase withmain

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,11 +16,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "test to confirm with valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
+<<<<<<< HEAD
       post users_path, params: { user: {
                                 name: "Example User",
                                 email: "user@example.com",
                                 password: "password",
                                 confirm_password: "password" } }
+=======
+      post users_path, params: params
+>>>>>>> 9794bf7 (Update test/integration/users_signup_test.rb)
     end
     follow_redirect!
     assert_template 'users/show'
