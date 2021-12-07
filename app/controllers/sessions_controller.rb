2fd1d553fd8fef4b4cf6b1adb822a3682457c5b8
kalpeshmithal <kalpesh@docfox.io>
2021-12-07 08:14:56 +0200

    fixed failing test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,24 +30,6 @@ class SessionsController < ApplicationController
         render 'new'
     end
   end
-  # Returns the current logged-in user (if any).
-  def current_user
-    if (user_id = session[:user_id])
-      user ||= User.find_by(id: user_id)
-        @current_user||= user if (user.session_token!=session['session_token'])
-    elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
-  # Returns true if the given user is the current user.
-  def current_user?(user)
-    user && user == current_user
-  end
 
   def destroy
     log_out if logged_in?
