02f3d56a092f104c4185a4ec5dd2167968043a31
kalpeshDocFox <93598787+kalpeshDocFox@users.noreply.github.com>
2021-12-14 08:02:32 +0200

    Update test/integration/users_signup_test.rb
    Co-authored-by: Chris <36987322+c13e@users.noreply.github.com>

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -33,13 +33,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information with account activation" do get signup_path
     assert_difference 'User.count', 1 do
-    post users_path, params: { user: 
-                                { name: "Example User",
-                                  email: "user@example.com",
-                                  password:              "password",
-                                  password_confirmation: "password" 
-                                } 
-                              }
+      params = {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+      post users_path, params
     end
     assert_equal 1, ActionMailer::Base.deliveries.size 
     user = assigns(:user)
